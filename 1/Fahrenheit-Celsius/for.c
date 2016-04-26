eeba5c49bda7f59abadeb134d4adf99779f586f5
ning1635 <609329119@qq.com>
2016-04-26 09:45:55 +0800

    Added files via upload

---
+++ 1/Fahrenheit-Celsius/for.c
@@ -0,0 +1,13 @@
+/*
+ * for循环打印华氏转温度
+ *
+ */
+#include <stdio.h>
+void main(){
+	float fahr;        /* 华氏 */
+	float celsius;     /* 温度 */
+
+	for(fahr = 0;fahr < 300;fahr = fahr + 20){
+		printf("%3.0f%6.1f\n",fahr,(fahr - 32.0) * 5.0 / 9.0 );
+	}
+}

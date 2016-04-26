dfa32f769f2f66484ea84312c1e2b8ee7ade40cc
ning1635 <609329119@qq.com>
2016-04-26 18:51:07 +0800

    空格替换成少量的制表符

--- 1/exercise/entab.c
+++ 1/exercise/entab.c
@@ -1,3 +1,44 @@
 /*
  * 空格串替换成最少数量的制表符和空格，但要保持单词之间的空格不变。
  */
+
+#include <stdio.h>
+
+void main(){
+	FILE * finp;
+	finp = fopen("1.txt","r");
+	int ch;
+	ch = getc(finp);
+	int flag = 0;//字符标号
+	while(!feof(finp)){
+		if(ch == '\n'){
+			flag = 0;
+		}
+		if(ch == ' ' && flag == 0){
+			int tFlag = 0;
+			while(ch == ' '){
+				if(tFlag == 8){
+					printf("\t");
+					tFlag = 0;
+				}
+				else{
+					tFlag++;
+				}
+				ch = getc(finp);
+			}
+			
+			int i;
+			for(i = tFlag;i > 0;i--){
+				printf(" ");
+			}
+		}
+		else{
+			printf("%c",ch);
+			flag++;
+			if(flag == 8){
+				flag = 0;
+			}
+		}
+		ch = getc(finp);
+	}
+}

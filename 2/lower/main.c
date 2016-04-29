45a7224354f266f5b473df99756677d25e7330a1
ning1635 <609329119@qq.com>
2016-04-29 14:27:15 +0800

    Update main.c

--- 2/lower/main.c
+++ 2/lower/main.c
@@ -1,3 +1,23 @@
 /*
  * 转换成小写字母
  */
+/*
+ * 将字符转换成小写字母
+ */
+
+#include <stdio.h>
+void lower(char *);
+void main(){
+	char s[10];
+	scanf("%s",s);
+	lower(s);
+	printf("%s",s);
+}
+void lower(char *s){
+	while(*s != '\0'){
+		if(*s > 'A' && *s < 'Z'){
+			*s +=0x20; 
+		}
+		s++;
+	}
+}

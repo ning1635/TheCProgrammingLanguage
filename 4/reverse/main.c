976ec7f470694007f2ac2dc77c1bd0cca73542f8
ning1635 <609329119@qq.com>
2016-05-05 19:33:04 +0800

    Update main.c

--- 4/reverse/main.c
+++ 4/reverse/main.c
@@ -1,1 +1,26 @@
-reverse(s) 将s字符串倒置
+/*
+ * 将字符串反转
+ */
+
+#include <stdio.h>
+
+void revers(char *);
+
+int main(){
+	char s[100];
+	scanf("%s",s);
+	revers(s);
+	return 1;
+}
+
+void revers(char *s){
+	char *temp = s;
+	for(;*s != '\0';s++)
+		;
+	s--;
+	while(s != temp){
+		printf("%c",*s);
+		s--;
+	}
+	printf("%c",*s);
+}

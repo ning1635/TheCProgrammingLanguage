f0f55ac5d805d3771cb7948d4d8cc0ffddf6ceb0
ning1635 <609329119@qq.com>
2016-05-06 19:37:23 +0800

    Update calc.c

--- 4/calc/calc.c
+++ 4/calc/calc.c
@@ -8,12 +8,26 @@
 #include <stdio.h>
 
 char inputBuf[100];      //用户输入
-char chageBug[100];
+char changeBuf[100];
 
-void change(char *,char*);   //中序转后序（逆波兰）
+char stack[10];
+char *sp = stack - 1;
+char *spbak = stack -1;
+char *out = changeBuf;
+
+void change(char *);   //中序转后序（逆波兰）
+void pushChangeBuf(char *p);
+int priority(char *p);
+
 int main(){
         scanf("%s",inputBuf);
-        printf("%s",inputBuf);
+	//stack初始化
+	int initStack = 0;
+	for(;initStack < 10;initStack++){
+		stack[initStack] = '\0';
+	}
+	change(inputBuf);
+        printf("%s\n",changeBuf);
         return 1;
 }       
 
@@ -21,8 +35,17 @@ int main(){
  * 中序转后序
  * 1+1  1+2*3  1*3+2  (1+2)*3
  */
-void change(char *s1,char *s2){
-        
+void change(char *s1){
+	while(*s1 != '\0'){
+		pushChangeBuf(s1);
+		s1++;
+	}
+	//弹出stack
+	while(spbak != sp){
+		*out = *sp;
+		out++;
+		sp--;
+	}
 }       
 /*
  * 递归把中序转成后序
@@ -30,4 +53,28 @@ void change(char *s1,char *s2){
  * 只做+ - * /
  * ()用递归实现
  */
-void pushChageBuf(float number,char
+void pushChangeBuf(char *p){
+	if(*p == '+' || *p == '-' || *p == '*' || *p == '/'){
+		//当前op优先级小于等于stack的优先级时，stack弹出到out
+		if(*sp != '\0' && priority(p)){
+			*out = *sp;
+			*sp = *p;
+			out++;
+			return;
+		}	
+		sp++;
+		*sp = *p;
+	}
+	else{
+		*out = *p;
+		out++;
+	}
+}
+
+int priority(char *p){
+	//当前操作符是+- 或者上一次操作符是*/
+	if(*p == '+' || *p == '-' || *sp == '*' || *sp == '/'){
+		return 1;
+	}
+	return 0;
+}

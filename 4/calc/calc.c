f71e00797f67e13deee2ab56404b54f233d6b341
ning1635 <609329119@qq.com>
2016-05-09 19:23:19 +0800

    Update calc.c

--- 4/calc/calc.c
+++ 4/calc/calc.c
@@ -18,7 +18,7 @@ char *out = changeBuf;
 void change(char *);   //中序转后序（逆波兰）
 void pushChangeBuf(char *p);
 int priority(char *p);
-
+int outValue(char *);
 int main(){
         scanf("%s",inputBuf);
 	//stack初始化
@@ -27,10 +27,53 @@ int main(){
 		stack[initStack] = '\0';
 	}
 	change(inputBuf);
-        printf("%s\n",changeBuf);
+        printf("print:%s\n",changeBuf);
+	printf("%d\n",outValue(changeBuf));
         return 1;
 }       
 
+/*
+ * 输出
+ *
+ */
+int outValue(char *p){
+	int ret = 0;
+	while(*p != '\0'){
+		if(*p == '+'){
+			//printf("ret:%d,*sp:%d\n",*sp - '0' + *(sp - 1) - '0',*sp - '0');
+			printf("%s\n",spbak +1);
+		    	ret = *sp - '0' + *(sp - 1) - '0';   
+			printf("ret:%d\n",ret);
+			sp = sp - 1;
+			*sp = ret + '0';
+		}
+		else if(*p == '-'){
+			printf("%s\n",spbak + 1);
+		    	ret = *(sp - 1) - '0' - (*sp - '0');
+			printf("ret:%d\n",ret);
+			sp = sp - 1;
+			*sp = ret + '0';
+		}
+		else if(*p == '*'){
+			printf("%s\n",spbak+1);
+			ret = (*sp - '0') * (*(sp -1) - '0');
+			sp = sp - 1;
+			*sp = ret + '0';
+			printf("%d",ret);
+		}
+		else if(*p == '/'){
+			ret = (*(sp - 1) - '0') / (*sp - '0');
+			sp = sp - 1;
+			*sp = ret + '0';
+		}
+		else{
+			sp++;
+			*sp = *p;
+		}
+		p++;
+	}
+	return ret;
+}
 /*
  * 中序转后序
  * 1+1  1+2*3  1*3+2  (1+2)*3
@@ -50,8 +93,8 @@ void change(char *s1){
 /*
  * 递归把中序转成后序
  * 
- * 只做+ - * /
- * ()用递归实现
+ * 如果是操作数，直接输出 
+ * 
  */
 void pushChangeBuf(char *p){
 	if(*p == '+' || *p == '-' || *p == '*' || *p == '/'){
@@ -65,6 +108,18 @@ void pushChangeBuf(char *p){
 		sp++;
 		*sp = *p;
 	}
+	else if(*p == ')'){
+		while(*sp != '('){
+			*out = *sp;
+			sp--;
+			out++;
+		}
+		sp--;
+	}
+	else if(*p =='('){
+		sp++;
+		*sp = *p;
+	}
 	else{
 		*out = *p;
 		out++;
@@ -72,6 +127,9 @@ void pushChangeBuf(char *p){
 }
 
 int priority(char *p){
+	if(*sp == '('){
+		return 0;
+	}
 	//当前操作符是+- 或者上一次操作符是*/
 	if(*p == '+' || *p == '-' || *sp == '*' || *sp == '/'){
 		return 1;

f478b41e8d06b99616adf9520c6f2863c01413e0
ning1635 <609329119@qq.com>
2016-05-10 12:15:23 +0800

    Update main.c

--- 5/echo/main.c
+++ 5/echo/main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 int main(int argc,char *argv[]){
-//	printf("argc:%d\n",argc);
-//	if(argc >= 2){
-//		char **p = argv;
-//		while((*p)++ <= argv[argc - 1]){
-//			printf("%s",*(*p));
-//		}
-//	}
-	int i = 1;
-	for(;i < argc;i++){
-		printf("%s%c",argv[i],(i == (argc - 1) ? '\0':32));
+	printf("argc:%d\n",argc);
+	if(argc >= 2){
+		char **p = argv;
+		printf("%s\n",*(p+1));
+		printf("%d\n",argv[argc - 1] - *p);
+		while(*p++ - argv[argc - 1] != 0){
+			printf("%s",*p);
+		}
 	}
-	return 1;
+//	int i = 1;
+//	for(;i < argc;i++){
+//		printf("%s%c",argv[i],(i == (argc - 1) ? '\0':32));
+//	}
+//	return 1;
 }

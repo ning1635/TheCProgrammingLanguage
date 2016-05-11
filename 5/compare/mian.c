f0ff692b07bf40c07d53552c33aa85484a82ad1a
ning1635 <609329119@qq.com>
2016-05-11 20:00:50 +0800

    Update mian.c

--- 5/compare/mian.c
+++ 5/compare/mian.c
@@ -11,6 +11,8 @@
 
 int numberComp(void *,void *);
 void input();
+void swap(char *,char *);
+
 char *linesPrt[MAXLINE];
 int main(int argc,char *argv[]){
 	input();
@@ -35,8 +37,17 @@ void input(){
 		//printf("%s",*(p - 1));
 	}while(1);
 	//printf("%d",*linesPrt[1]);
+	swap(*p,*(p+1));
 	int a = 0;
 	while(*p != linesPrt[a] ){
 		printf("%d:%s\n",a,linesPrt[a++]);
 	}
+	//printf("%d\n",strcmp(*p,*(p+1)));
+}
+
+void swap(char *s,char *p){
+	char *temp;
+	temp = s;
+	s = p;
+	p = temp;
 }

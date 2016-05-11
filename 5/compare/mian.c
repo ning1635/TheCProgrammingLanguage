0a9806823b7c77acc88a89b82ec4c5b591f3f72c
ning1635 <609329119@qq.com>
2016-05-11 23:42:01 +0800

    Update mian.c

--- 5/compare/mian.c
+++ 5/compare/mian.c
@@ -6,16 +6,21 @@
 
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 #define MAXLINE 100
 
 int numberComp(void *,void *);
 void input();
-void swap(char *,char *);
+void compare();
+void printResult();
+void swap(char *p[],int i,int j);
 
-char *linesPrt[MAXLINE];
+char *linesPrt[MAXLINE];//保存输入的内容
+char **p = linesPrt;//输入结尾指针
 int main(int argc,char *argv[]){
 	input();
+	compare();
+	printResult();
 	//处理输入的参数
 	//接收输入的内容
 	//
@@ -25,7 +30,7 @@ int main(int argc,char *argv[]){
  * input
  */
 void input(){
-	char **p = linesPrt;
+	//char **p = linesPrt;
 	char *temp;
 	do{
 		temp = (char *)malloc(sizeof(char)*1000);
@@ -37,17 +42,36 @@ void input(){
 		//printf("%s",*(p - 1));
 	}while(1);
 	//printf("%d",*linesPrt[1]);
-	swap(*p,*(p+1));
+	//swap(linesPrt,0,1);
+	//printf("%d\n",strcmp(*p,*(p+1)));
+}
+/*
+ * 冒泡
+ */
+void compare(){
+	int a = 0;
+	int sub = p - linesPrt;
+	int i = sub - 1;
+	while(*p != linesPrt[a]){
+		for(;i > a;i--){
+			if(strcmp(linesPrt[i],linesPrt[i - 1]) < 0){
+				swap(linesPrt,i,i-1);
+			}
+		}
+		a++;
+		i = sub -1;
+	}
+}
+void printResult(){
 	int a = 0;
 	while(*p != linesPrt[a] ){
 		printf("%d:%s\n",a,linesPrt[a++]);
 	}
-	//printf("%d\n",strcmp(*p,*(p+1)));
-}
 
-void swap(char *s,char *p){
+}
+void swap(char *p[],int i,int j){
 	char *temp;
-	temp = s;
-	s = p;
-	p = temp;
+	temp = p[i];
+	p[i] = p[j];
+	p[j] = temp;
 }

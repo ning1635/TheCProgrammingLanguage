80d278aff715673ba579fd88efb6350a45cdcf55
ning1635 <609329119@qq.com>
2016-05-12 19:16:29 +0800

    函数指针

--- 5/compare/mian.c
+++ 5/compare/mian.c
@@ -2,6 +2,7 @@
  * 排序输入的内容
  * 支持字符串、数字
  * -n数字 -s字符串
+ * 指向函数的指针区别ns
  */
 
 #include <stdio.h>
@@ -11,15 +12,25 @@
 
 int numberComp(void *,void *);
 void input();
-void compare();
+void compare(int (*cmp)(void *,void *));
 void printResult();
 void swap(char *p[],int i,int j);
+int numcmp(char *,char *);
 
 char *linesPrt[MAXLINE];//保存输入的内容
 char **p = linesPrt;//输入结尾指针
 int main(int argc,char *argv[]){
+	//(int (*)(void *,void *)) cmp = strcmp;
+	//处理参数，判断用哪个方法比较
+	int flag = 1;
+	if(argc > 1){
+		char *param = argv[1];
+		if(*param == '-' && *(param + 1) == 's'){
+			flag = 0;
+		}
+	}
 	input();
-	compare();
+	compare(flag ? (int (*)(void *,void *))numcmp : (int (*)(void *,void *))strcmp);
 	printResult();
 	//处理输入的参数
 	//接收输入的内容
@@ -48,13 +59,13 @@ void input(){
 /*
  * 冒泡
  */
-void compare(){
+void compare(int (*cmp)(void *,void *)){
 	int a = 0;
 	int sub = p - linesPrt;
 	int i = sub - 1;
 	while(*p != linesPrt[a]){
 		for(;i > a;i--){
-			if(strcmp(linesPrt[i],linesPrt[i - 1]) < 0){
+			if(cmp(linesPrt[i],linesPrt[i - 1]) < 0){
 				swap(linesPrt,i,i-1);
 			}
 		}
@@ -74,4 +85,17 @@ void swap(char *p[],int i,int j){
 	temp = p[i];
 	p[i] = p[j];
 	p[j] = temp;
+}
+int numcmp(char *s1,char *s2){
+	float f1 = atof(s1);
+	float f2 = atof(s2);
+	if(f1 > f2){
+		return 1;
+	}
+	else if(f1 == f2){
+		return 0;
+	}
+	else if(f1 < f2){
+		return -1;
+	}
 }
